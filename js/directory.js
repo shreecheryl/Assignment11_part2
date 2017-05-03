272a90f42e44831183f43c2cce7ef4ced8500843
shreecheryl <shreecheryl@att.net>
2017-05-03 12:46:01 -0700

    in progress 050317

--- js/directory.js
+++ js/directory.js
@@ -1,4 +1,4 @@
-$(document).on('pagecreate', '#directory', function () {
+$(function () {
     "use strict";
     $('#autocomplete').on('keyup', function () {
         var userInput = $('#autocomplete').val(),
--- js/employeeDetails.js
+++ js/employeeDetails.js
@@ -1,22 +1,29 @@
-$(document).on('pagecreate', '#employeeDetails', function (event, data) {
+$(function () {
     "use strict";
-    var id = event.target.title;
-    window.console.log(id);
-//        $.ajax({
-//            url: "data/json.js",
-//            dataType: "json",
-//            success: function (response) {
-//                $('#autocomplete-input').html('');
-//				$.each(response, function () {
-//                    $.each(this, function (key, value) {
-//                        if (forSearch.test(value.name)) {
-//                            listItems += '<li><a href="#employeeDetails" title=' + value.employeeId + '><img src=' + value.imagePath + '>' + value.name + '<p>' + value.title + '</p>' + '</a></li>';
-//                            $("#autocomplete-input").html(listItems);
-//                        }
-//                        $('#autocomplete-input').listview('refresh');
-//                        $('#autocomplete-input').trigger('updatelayout');
-//                    });
-//                });
-//			}
-//		});
+    $("#autocomplete-input").on("click", function (event) {
+        var id = event.target.title,
+            $nameTitle = $("#nameTitle"),
+            nameTitleInput = '';
+        $.ajax({
+            url: "data/json.js",
+            dataType: "json",
+            success: function (response) {
+                $nameTitle.html('');
+				$.each(response, function () {
+                    $.each(this, function (key, value) {
+                        if (value.employeeId === id) {
+                            nameTitleInput += '<img src=' + value.imagePath + '><h3>' + value.name + '</h3><p>' + value.title + '</p>';
+                            $nameTitle.html(nameTitleInput);
+                            $("#manager p").text(value.ReportsTo);
+                            $("#directReports p").text("toDo");
+                            $("#officePhone p").text(value.officeNumber);
+                            $("#officePhone a").attr("href", "tel:" + value.officeNumber);
+                            $("#cellPhone p").text(value.cellNumber);
+                            $("#cellPhone a").attr("href", "tel:" + value.cellNumber);
+                        }
+                    });
+                });
+			}
+		});
+    });
 });
